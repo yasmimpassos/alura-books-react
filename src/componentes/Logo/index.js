104ed7f19139e3ff70be8f90e942b2db5bdcbddb
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2024-11-22 20:18:13 -0300

    feat: retira os arquivos css adiciona as estilizações dentro do proprio arquivo js usando styled components

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,12 +1,21 @@
-import './estilo.css'
+import styled from 'styled-components'
 import logo from '../../imagens/logo.svg'
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+`
 
 function Logo() {
   return (
-    <div className='logo'>
-      <img src={logo} alt='logo' className='logo-img'></img>
+    <LogoContainer>
+      <LogoImage src={logo} alt='logo'/>
       <p><strong>Alura</strong> Books</p>
-    </div>
+    </LogoContainer>
   )
 }
 
--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,15 +1,31 @@
-import './estilo.css'
+import styled from 'styled-components'
+
+const Opcao = styled.li`
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
+
+const Opcoes = styled.ul`
+    display: flex;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
-  return (
-    <ul className='opcoes'>
-      {textoOpcoes.map((texto) => (
-        <li className='opcao'><p>{texto}</p></li>
-      ))}
-    </ul>
-  )
+    return (
+        <Opcoes>
+            { textoOpcoes.map ( (texto) => (
+                <Opcao><p>{texto}</p></Opcao>
+            ) ) }
+        </Opcoes>
+    )
 }
 
-export default OpcoesHeader;
+export default OpcoesHeader
