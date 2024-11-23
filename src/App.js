ad9db3a1936b255a6b971462368b0f89a81fed4e
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2024-11-22 22:11:12 -0300

    feat: adiciona a seção de ultimos lançamentos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './componentes/Header';
 import styled from 'styled-components';
 import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -13,6 +14,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
